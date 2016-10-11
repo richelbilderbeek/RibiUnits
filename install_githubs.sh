8f9a67e52dc81712c45357060b0a7b9cd2663b60
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-11 20:46:40 +0200

    Improved Travis script

--- install_githubs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ..
-#git clone https://github.com/richelbilderbeek/BoostGraphTutorial
-git clone https://github.com/richelbilderbeek/RibiClasses
-git clone https://github.com/richelbilderbeek/RibiLibraries
-git clone https://github.com/richelbilderbeek/QtKeyboardFriendlyGraphicsView
-
